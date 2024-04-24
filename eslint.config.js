df599dd14063b6e75271f39a5b362ed1b9de5a50
Carlos Felgueiras <catofel2000@gmail.com>
2024-04-24 02:49:24 +0100

    chore(prettier): added prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import prettierConfig from "eslint-config-prettier";
 
 pluginReactConfig.rules["react/react-in-jsx-scope"] = "off";
 
@@ -9,6 +10,7 @@ export default [
   { ignores: ["dist/"] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   { settings: { react: { version: "detect" } } },
+  prettierConfig,
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
